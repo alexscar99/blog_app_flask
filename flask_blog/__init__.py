007a1f553843fa6dfd716581bdd5ca42f4465302
Alex Scarlett <alex.scarlett91@gmail.com>
2018-11-26 01:30:24 -0500

    Restructured app into a package from single module

---
+++ flask_blog/__init__.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', default=None)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from flask_blog import routes
