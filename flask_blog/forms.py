ec9b1413d7d725050004bfa82d5e4c9d3b225d75
Alex Scarlett <alex.scarlett91@gmail.com>
2018-11-27 01:41:15 -0500

    users can now successfully reset their passwords via an email sent with a token

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -22,8 +22,8 @@ class RegistrationForm(FlaskForm):
                 'The username you entered already exists. Please enter another one')
 
     def validate_email(self, email):
-        email = User.query.filter_by(email=email.data).first()
-        if email:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
             raise ValidationError(
                 'The email you entered already exists. Please enter another one')
 
@@ -52,8 +52,8 @@ class UpdateAccountForm(FlaskForm):
 
     def validate_email(self, email):
         if email.data != current_user.email:
-            email = User.query.filter_by(email=email.data).first()
-            if email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
                 raise ValidationError(
                     'The email you entered already exists. Please enter another one')
 
@@ -62,3 +62,21 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError(
+                'There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
